acf4cc7a7802b18bc83d59634d4b032e83cddb54
Infamerous <138701740+Infamerous@users.noreply.github.com>
2025-11-13 22:57:07 +0700

    Add files via upload

---
+++ week2_schema_SQL/libraryBorrowingTracker_ddl.sql
@@ -0,0 +1,62 @@
+create database libraryBorrowingTracker;
+use libraryBorrowingTracker;
+
+CREATE TABLE Borrower
+(
+  userID INT NOT NULL,
+  borrowerName INT NOT NULL,
+  borrowerEmail INT NOT NULL,
+  borrowerPass INT NOT NULL,
+  PRIMARY KEY (userID)
+);
+
+CREATE TABLE Librarian
+(
+  librarianID INT NOT NULL,
+  librarianName INT NOT NULL,
+  librarianEmail INT NOT NULL,
+  librarianPass INT NOT NULL,
+  PRIMARY KEY (librarianID)
+);
+
+CREATE TABLE Book
+(
+  bookID INT NOT NULL,
+  bookISBN INT NOT NULL,
+  bookTitle INT NOT NULL,
+  bookGenre INT NOT NULL,
+  bookStatus INT NOT NULL,
+  PRIMARY KEY (bookID)
+);
+
+CREATE TABLE bookAuthor
+(
+  bookAuthor INT NOT NULL,
+  bookID INT NOT NULL,
+  PRIMARY KEY (bookAuthor, bookID),
+  FOREIGN KEY (bookID) REFERENCES Book(bookID)
+);
+
+CREATE TABLE Borrowing
+(
+  borrowID INT NOT NULL,
+  borrowDate INT NOT NULL,
+  returnDate INT NOT NULL,
+  fine INT,
+  bookID INT NOT NULL,
+  userID INT NOT NULL,
+  librarianID INT NOT NULL,
+  PRIMARY KEY (borrowID),
+  FOREIGN KEY (bookID) REFERENCES Book(bookID),
+  FOREIGN KEY (userID) REFERENCES Borrower(userID),
+  FOREIGN KEY (librarianID) REFERENCES Librarian(librarianID)
+);
+
+CREATE TABLE Manages
+(
+  librarianID INT NOT NULL,
+  bookID INT NOT NULL,
+  PRIMARY KEY (librarianID, bookID),
+  FOREIGN KEY (librarianID) REFERENCES Librarian(librarianID),
+  FOREIGN KEY (bookID) REFERENCES Book(bookID)
+);
